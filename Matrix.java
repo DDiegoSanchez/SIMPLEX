0653d9808ee4d2cfedaa77f9b81e850bf1253842
DDiegoSanchez <dayan.ddsr@gmail.com>
2021-11-24 15:03:58 -0600

    update repo

--- Matrix.java
+++ Matrix.java
@@ -8,7 +8,6 @@
 //Class to print the array
 public class Matrix {
     public static void printMatrix(int colums, int rows,int matrix[][]){
-        
         //Print slack variables
         System.out.print("SV");
 
@@ -37,27 +36,38 @@ public class Matrix {
                 //Objective Function
                 System.out.print("Z");
             }
-
+            
+            //For to create the matrix
             for(int j=0; j<(colums+rows+1); j++){
                 System.out.print("\t" + matrix[i][j] + " ");
             }
-        }
 
         System.out.print("\n");  
-        
+        }
         //Identify input column
         int less=matrix[rows][colums];
         int column=0;
-            for(int i=0; i < (rows+1); i++){
-                for(int j=0; j<(colums+rows+1);j++){
-                    if(matrix[i][j] < less){
-                        less = matrix[i][j];
-                        column = j;
-                    }
-                }
+        for(int j=0;j<matrix[matrix.length-1].length;j++) {
+            if(matrix[matrix.length-1][j] < less){
+                less = matrix[matrix.length-1][j];
+                column = j;
             }
+        }
+        System.out.print("\n");
 
         System.out.print("\nThe lower value is: "+ less);
         System.out.print("\nThe input column is: "+ column);
+        System.out.print("\n"); 
+        
+        //Print input column
+        for(int i=0; i<matrix.length; i++){
+            System.out.print("\n" + matrix[i][column] + " ");
+        }
+        
+        //Column result
+        System.out.print("\n");
+        for(int z=0; z<((rows+colums)); z++){
+            System.out.print("\n" +matrix[z][(rows+colums)] + " ");
+        }
     } 
 }
